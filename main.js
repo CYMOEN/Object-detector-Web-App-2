f55ac1235e2f997e7617406c5afbfa8f028cffbc
CYMOEN <121981493+CYMOEN@users.noreply.github.com>
2024-05-17 18:46:52 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -11,8 +11,6 @@ function setup() {
     video = createCapture(VIDEO);
     video.size(380,380)
     video.hide();
-   }
-function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
